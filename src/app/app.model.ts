39eea160b29d885e614b1a6f2b2fee306360a0a1
savanyab <savanyab@gmail.com>
2018-12-13 10:31:02 +0100

    added Category enum

--- src/app/app.model.ts
+++ src/app/app.model.ts
@@ -3,9 +3,17 @@ export class AppModel {
     constructor (
         public title?: string, //?: nem kötelező megadni azt a változót (akkor undefinedra inicializálódnak)
         public checked?: boolean,
-        public category?: string,
-        public text?: string
-    ) {
-
+        public category?: Category,
+        public text?: string) {
+        
     }
+
+    
+}
+
+export enum Category {
+    BILLING ='BILLING', 
+    HOUSE = 'HOUSE', 
+    SHOPPING = 'SHOPPING', 
+    TRAVEL = 'TRAVEL'
 }
