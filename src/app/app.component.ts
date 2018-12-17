99d92efc5a662ce366bb8379a93c00ff58154780
savanyab <savanyab@gmail.com>
2018-12-17 20:57:39 +0100

    login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,42 @@ export class AppComponent {
 
   public appImpl: AppInterface = <AppInterface>{};
 
-  constructor () {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  isValidName(name) {
+    const format = /^[A-Z]/;
+    return format.test(name);
+  }
+
+  isValidEmail(email) {
+    const format = /[a-zA-Z0-9\.-_]+@[a-zA-Z0-9]+\.com$/;
+    return format.test(email);
+  }
+  
+  ratePassword(password) {    
+    const weakFormat = /[a-z0-9]{1,7}/;
+    const mediumFormat = /[a-zA-Z0-9]{8,13}/;
+    const strongFormat = /[a-zA-Z0-9$@&#]{14,}/;
+    if (weakFormat.test(password)) {
+      return 'red';
+    } else if (mediumFormat.test(password)) {
+      return 'orange';
+    } else if (strongFormat.test(password)) {
+      return 'green';
+    };
     
+  }
+
+
+  isValidPassword(password) {
+    return this.ratePassword(password) === 'green';
+  }
+
+
+  constructor() {
+
   }
 
   public showAppModelValue() { // ha nem írom ki hogy public, a default akkor is az
@@ -27,5 +61,7 @@ export class AppComponent {
       this.appModel.checked = false;
     }*/
   }
+
+
 
 }
